6534a10434e014739680103c4f44de59add6db39
Klaus Jørgensen <klaus@pnorental.com>
2022-09-23 00:24:46 +0200

    Navigation now fully relys on bootstrap-vue, no more need for jquery

--- vue/src/components/Navigation.vue
+++ vue/src/components/Navigation.vue
@@ -1,38 +1,33 @@
 <template>
   <header>
-    <nav class="navbar navbar-expand-md fixed-top bg-dark">
-      <div class="container-fluid h-100 p-0">
-        <a class="navbar-brand" href="./"><i class="fas fa-moon fa-flip-vertical" :class="[statusClass]"></i> Nathejk</a>
-        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarCollapse" aria-controls="navbarCollapse" aria-expanded="false" aria-label="Toggle navigation">
-          <span class="navbar-toggler-icon"></span>
-        </button>
-        <div class="collapse navbar-collapse" id="navbarCollapse">
-            <ul class="navbar-nav me-auto mb-2 mb-lg-0 nav nav-tabs">
-              <li class="nav-item">
-                <router-link :to="{name:'home'}" class="nav-link mb-0" active-class="active" exact>
-                  <i class="fas fa-lg fa-fw fa-home"></i><small>Hjem</small>
-                </router-link>
-              </li>
-              <li class="nav-item" v-for="(m, i) in menu" :key="i">
-                  <a v-if="m.link.substring(0, 4) == 'http'" class="nav-link mb-0" :href="m.link">
-                    <i class="fas fa-lg fa-fw" :class="m.icon"></i><small>{{ m.label }}</small> {{ m.text}}
-                  </a>
-                  <router-link v-else :to="m.link" class="nav-link mb-0" active-class="active" __exact>
-                    <i class="fas fa-lg fa-fw" :class="m.icon"></i><small>{{ m.label }}</small> {{ m.text}}
-                  </router-link>
-              </li>
-            </ul>
-          <ul class="navbar-nav ml-auto mb-2 mb-lg-0" old="navbar-nav ml-auto navr-right">
-            <li class="nav-item dropdown">
-              <a class="nav-link dropdown-toggle" href="http://example.com" id="dropdown01" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false"><i class="far fa-user"></i> {{ user.name }}</a>
-              <div class="dropdown-menu dropdown-menu-right" aria-labelledby="dropdown01">
-                <a class="dropdown-item" href="http://lukmigind.nathejk.dk/logout"><i class="fas fa-sign-out-alt fa-fw"></i> Log ud</a>
-              </div>
-            </li>
-          </ul>
-        </div>
-      </div>
-    </nav>
+
+    <b-navbar toggleable="lg" type="dark" fixed="top">
+
+      <a class="navbar-brand" href="./"><i class="fas fa-moon fa-flip-vertical" :class="[statusClass]"></i> Nathejk</a>
+      <b-navbar-toggle target="nav-collapse"></b-navbar-toggle>
+
+      <b-collapse id="nav-collapse" is-nav>
+        <b-navbar-nav class="tabs">
+          <b-nav-item :to="{name:'home'}" active-class="active" exact>
+            <i class="fas fa-fw fa-home"></i><small>Hjem</small>
+          </b-nav-item>
+          <b-nav-item v-for="(m, i) in menu" :key="i" :to="m.link" :href="m.href" active-class="active">
+            <i class="fas fa-fw" :class="m.icon"></i><small>{{ m.label }}</small>{{ m.text}}
+          </b-nav-item>
+        </b-navbar-nav>
+
+        <b-navbar-nav class="ml-auto">
+          <b-nav-item-dropdown right>
+            <template #button-content>
+              <i class="far fa-user fa-fw"></i> {{ user.name }}
+            </template>
+            <b-dropdown-item href="http://lukmigind.nathejk.dk/logout"><i class="fas fa-sign-out-alt fa-fw"></i> Log ud</b-dropdown-item>
+          </b-nav-item-dropdown>
+        </b-navbar-nav>
+      </b-collapse>
+
+    </b-navbar>
+
   </header>
 </template>
 
@@ -41,7 +36,7 @@ header {
     padding-top:60px;
 }
 nav.navbar {
-    height: 60px; 
+    min-height: 60px;
     padding:0 1rem;
     background-image: linear-gradient(to bottom,#445e65 0,#1f2a26 100%);
     background-repeat: repeat-x;
@@ -50,66 +45,82 @@ nav a.navbar-brand {
     font: bold 30px/60px 'helvetica neue',helvetica,arial,sans-serif;
     text-transform: uppercase;
     color: white;
+    padding:0;
 
     i {
         font-size:1.2rem;
         vertical-align: text-top;
     }
 }
-nav .navbar-collapse { padding-top:.5rem;
-  align-self: flex-end;
+nav .navbar-collapse {
+    padding-top:.5rem;
+    align-self: flex-end;
 }
-nav ul.nav { border-bottom: 0;
-  align-self: flex-end;
+nav ul { border-bottom: 0;
+    align-self: flex-end;
 }
-nav ul.nav li.nav-item {margin-right:.2rem; margin-bottom:0;}
-nav ul.nav li.nav-item a {
-    /*background-image: linear-gradient(to bottom,#1f2a26 0,#445e65 100%);*/
-    background-color:#445e65;
-    background-repeat: repeat-x;
-    border-top-left-radius:.5rem;
-    border-top-right-radius:.5rem; 
-    color:rgba(255,255,255,.5);
-    padding:.25rem .5rem;
+#nav-collapse:not(.show) .tabs li.nav-item {
 
-    &:hover small {
-        display:inline;
+    margin-right:.2rem;
+    margin-bottom:0;
+
+    a {
+        /*background-image: linear-gradient(to bottom,#1f2a26 0,#445e65 100%);*/
+        background-color:#445e65;
+        background-repeat: repeat-x;
+        border-top-left-radius:.5rem;
+        border-top-right-radius:.5rem; 
+        color:rgba(255,255,255,.5);
+        padding:.25rem .5rem;
+
+        &:hover small {
+            display:inline;
+        }
+        i {
+            font-size: 1.33333em;
+            line-height: .75em;
+            vertical-align: -0.0667em;
+        }
+        small {
+            position: absolute;
+            width: 58px;
+            text-align: center;
+            margin-left: -42px;
+            font-size: 8px;
+            margin-top: 28px;
+            text-transform: uppercase;
+            display:none;
+            color:#445e65;
+        }
     }
-}
-nav li.nav-item a {
-    color:rgba(255,255,255,.5);
-}
-nav ul.nav li.nav-item a.active {
-    background:#fff;
-    
-    small {
-        display:inline;
-    }
-}
-nav ul.nav li.nav-item a small {
-    position: absolute;
-    width: 58px;
-    text-align: center;
-    margin-left: -42px;
-    font-size: 8px;
-    margin-top: 28px;
-    text-transform: uppercase;
-    display:none;
+    a.active {
+        background:#fff;
         color:#445e65;
+        small {
+            display:inline;
+        }
+    }
 }
-
+#nav-collapse.show li.nav-item small {
+    padding-left:10px;
+}
 @media (min-width: 992px) {
   .modal-lg {
     max-width: 80%;
   }
 }
+unused {
+}
 </style>
 
 <script>
+import { BNavbar, BCollapse, BNavbarToggle, BNavItemDropdown, BDropdownItem } from 'bootstrap-vue'
 export default {
     props: {
         title: null
     },
+    components: { BNavbar, BCollapse, BNavbarToggle, BNavItemDropdown, BDropdownItem },
+
     data: () => ({
         activeMenu: 'søg',
         menu: [
@@ -122,7 +133,7 @@ export default {
             {icon:'fa-user-injured', label:'Udgået', link:'/ude'},
             {icon:'fa-fist-raised', label:'LOK', link:'/lok'},
             {icon:'fa-child', text:'Patruljer', link:'/patruljer'},
-            {icon:'fa-search', text:'Søg', link:'http://natpas.nathejk.dk/search'},
+            {icon:'fa-search', text:'Søg', href:'http://natpas.nathejk.dk/search'},
         ],
     }),
     computed: {
