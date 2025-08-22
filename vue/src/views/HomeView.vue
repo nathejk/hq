a46e19951d6a716ef824529676577407dfeefc33
Klaus Jørgensen <knj@sonarapp.com>
2025-08-22 14:08:26 +0200

    removed mockup data

--- vue/src/views/HomeView.vue
+++ vue/src/views/HomeView.vue
@@ -41,7 +41,7 @@ const load = async () => {
               <div class="flex items-center p-4">
                   <i class="pi pi-face-smile !text-5xl text-orange-500"></i>
                   <div class="ml-4">
-                      <span class="text-orange-500 block whitespace-nowrap">Tilmeldte gøglere</span>
+                      <span class="text-orange-500 block whitespace-nowrap uppercase">Tilmeldte gøglere</span>
                       <span class="text-orange-500 block text-4xl font-bold">{{ config.badutCount }}</span>
                   </div>
               </div>
@@ -49,9 +49,31 @@ const load = async () => {
           </div>
       </div>
 
-      <div class="col-span-12 md:col-span-6 xl:col-span-3"><div class="card !p-0 overflow-hidden flex flex-col"><div class="flex items-center p-4"><i class="pi pi-directions !text-5xl text-green-500"></i><div class="ml-4"><span class="text-green-500 block whitespace-nowrap">CONVERSION RATE</span><span class="text-green-500 block text-4xl font-bold">12.6%</span></div></div><img src="/dashboard/rate.svg" class="w-full" alt="conversion"></div></div>
+      <div class="col-span-12 md:col-span-6 xl:col-span-3">
+          <div class="card !p-0 overflow-hidden flex flex-col">
+              <div class="flex items-center p-4">
+                  <i class="pi pi-qrcode !text-5xl text-green-500"></i>
+                  <div class="ml-4">
+                      <span class="text-green-500 block whitespace-nowrap uppercase">Antal fangster</span>
+                      <span class="text-green-500 block text-4xl font-bold">0</span>
+                  </div>
+              </div>
+              <img src="/dashboard/rate.svg" class="w-full" alt="conversion">
+          </div>
+      </div>
 
-      <div class="col-span-12 md:col-span-6 xl:col-span-3"><div class="card h-full !p-0 overflow-hidden flex flex-col"><div class="flex items-center p-4"><i class="pi pi-comments !text-5xl text-purple-500"></i><div class="ml-4"><span class="text-purple-500 block whitespace-nowrap">ACTIVE TRIALS</span><span class="text-purple-500 block text-4xl font-bold">440</span></div></div><img src="/dashboard/interactions.svg" class="w-full mt-auto" alt="interactions"></div></div>
+      <div class="col-span-12 md:col-span-6 xl:col-span-3">
+          <div class="card h-full !p-0 overflow-hidden flex flex-col">
+              <div class="flex items-center p-4">
+                  <i class="pi pi-comments !text-5xl text-purple-500"></i>
+                  <div class="ml-4">
+                      <span class="text-purple-500 block whitespace-nowrap uppercase">Opkald til nødtelefon</span>
+                      <span class="text-purple-500 block text-4xl font-bold">0</span>
+                  </div>
+              </div>
+              <img src="/dashboard/interactions.svg" class="w-full mt-auto" alt="interactions">
+          </div>
+      </div>
 
       <!--
       <div class="col-span-12 xl:col-span-6"><div class="card"><div class="font-semibold text-xl mb-4">Monthly Recurring Revenue Growth</div><div class="p-chart" id="nasdaq-chart" data-pc-name="chart" pc73="" data-pc-section="root" style="position: relative;"><canvas width="1232" height="740" data-pc-section="canvas" style="display: block; box-sizing: border-box; height: 370px; width: 616px;"></canvas></div></div></div>
